1afbb5fba181487bfcc182bdea35b936d3a4676c
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-02 12:39:24 +0100

    Add takeStep and findRandomEmptyNeighbouringCell methods to Pokemon class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/pokemon/Pokemon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/pokemon/Pokemon.java
@@ -1,6 +1,7 @@
 package com.codecool.dungeoncrawl.logic.actors.pokemon;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Pokemon implements Drawable {
@@ -8,7 +9,7 @@ public abstract class Pokemon implements Drawable {
     private int pokeHealth;
     private int pokeDamage;
     private final String pokeName;
-    private Cell cell;
+    protected Cell cell;
 
     public Pokemon(Cell cell, String name){
         this.pokeName = name;
@@ -29,4 +30,24 @@ public abstract class Pokemon implements Drawable {
 
     public abstract void move();
     public abstract void fight();
+
+    public void takeStep(Cell moveTo){
+        cell.setPokemon(null);
+        moveTo.setPokemon(this);
+        cell = moveTo;
+    }
+    public Cell findRandomEmptyNeighbouringCell() {
+        int[] newCoordinates = new int[2];
+        while (true) {
+            int changes = (int) Math.round(Math.random());
+            int add = ((int) Math.round(Math.random()) == 0) ? -1 : 1;
+            newCoordinates[0] = (changes == 0) ? add : 0;
+            newCoordinates[1] = (changes == 0) ? 0 : add;
+            Cell neighbour = cell.getNeighbor(newCoordinates[0], newCoordinates[1]);
+            if (neighbour.getActor() == null && neighbour.getItem() == null && neighbour.getPokemon() == null
+                    && neighbour.getTileName().equals(CellType.FLOOR.getTileName())) {
+                return neighbour;
+            }
+        }
+    }
 }
