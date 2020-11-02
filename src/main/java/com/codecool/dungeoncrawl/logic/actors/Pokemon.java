ba937f218c573c8c2de2a560f6a8069889be6d2d
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-02 10:15:54 +0100

    Add Pokemon abstract class

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Pokemon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Pokemon.java
@@ -1,4 +1,25 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
-public class Pokemon {
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.Drawable;
+
+public abstract class Pokemon implements Drawable {
+    private boolean isActive = false;
+    private int pokeHealth;
+    private int pokeDamage;
+    private final String pokeName;
+
+    public Pokemon(Cell cell, String name){
+        this.pokeName = name;
+        this.pokeDamage = 2;
+        this.pokeHealth = 4;
+    }
+
+    @Override
+    public String getTileName() {
+        return "pokemon";
+    }
+
+    public abstract void move();
+    public abstract void fight();
 }
