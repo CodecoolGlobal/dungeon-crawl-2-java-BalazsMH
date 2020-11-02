f75b3fd62b63befa9307c51d7e26abfe648b1383
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-02 10:42:16 +0100

    Add cell field to Pokemon in constructor, getCell

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Pokemon.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Pokemon.java
@@ -8,16 +8,23 @@ public abstract class Pokemon implements Drawable {
     private int pokeHealth;
     private int pokeDamage;
     private final String pokeName;
+    private Cell cell;
 
     public Pokemon(Cell cell, String name){
         this.pokeName = name;
         this.pokeDamage = 2;
         this.pokeHealth = 4;
+        this.cell = cell;
+        this.cell.setPokemon(this);
     }
 
     @Override
     public String getTileName() {
         return "pokemon";
+    }
+
+    public Cell getCell() {
+        return cell;
     }
 
     public abstract void move();
