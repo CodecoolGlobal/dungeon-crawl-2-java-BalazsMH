c1ea0ad45e6ef032a9086e739203ba16f7d7dd14
BalazsMH <horvathb91@gmail.com>
2020-11-02 23:36:12 +0100

    Code refactored, disabled code removed.

--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -8,33 +8,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Tiles {
-    /*
-    public static int TILE_WIDTH = 32;
-
-    private static Image tileset = new Image("/tiles.png", 543 * 2, 543 * 2, true, false);
-    private static Map<String, Tile> tileMap = new HashMap<>();
-    public static class Tile {
-        public final int x, y, w, h;
-        Tile(int i, int j) {
-            x = i * (TILE_WIDTH + 2);
-            y = j * (TILE_WIDTH + 2);
-            w = TILE_WIDTH;
-            h = TILE_WIDTH;
-        }
-    }
-
-    static {
-        tileMap.put("empty", new Tile(0, 0));
-        tileMap.put("wall", new Tile(10, 17));
-        tileMap.put("floor", new Tile(2, 0));
-        tileMap.put("player", new Tile(27, 0));
-        tileMap.put("skeleton", new Tile(29, 6));
-        tileMap.put("lootbox", new Tile(12, 31));
-        tileMap.put("slowpoke", new Tile(28, 6));
-        tileMap.put("charizard", new Tile(27, 8));
-        tileMap.put("bulbasaur", new Tile(24, 8));
-
-    }*/
 
     public static int DEFAULT_TILE_WIDTH = 32;
 
