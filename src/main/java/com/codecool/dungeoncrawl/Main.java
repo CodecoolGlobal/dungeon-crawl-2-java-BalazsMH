df545a93c998b26d01ce7c80ca0708001ebe0452
BalazsMH <horvathb91@gmail.com>
2020-11-02 18:23:28 +0100

    TILE WIDTH constant renamed to DEFAULT_TILE_WIDTH

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -9,7 +9,6 @@ import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
@@ -22,8 +21,8 @@ import java.util.List;
 public class Main extends Application {
     GameMap map = MapLoader.loadMap();
     Canvas canvas = new Canvas(
-            map.getWidth() * Tiles.TILE_WIDTH,
-            map.getHeight() * Tiles.TILE_WIDTH);
+            map.getWidth() * Tiles.DEFAULT_TILE_WIDTH,
+            map.getHeight() * Tiles.DEFAULT_TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
 
@@ -80,7 +79,7 @@ public class Main extends Application {
     }
 
     private void refresh() {
-        context.setFill(Color.BLACK);
+        context.setFill(Color.GRAY);
         context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
         moveAllPokemon();
         for (int x = 0; x < map.getWidth(); x++) {
--- src/main/java/com/codecool/dungeoncrawl/Tiles.java
+++ src/main/java/com/codecool/dungeoncrawl/Tiles.java
@@ -36,18 +36,19 @@ public class Tiles {
 
     }*/
 
-    public static int TILE_WIDTH = 32;
+    public static int DEFAULT_TILE_WIDTH = 32;
 
     private static Image backgroundTileset = new Image("/emerald_rip.png", 1408 * 2, 1104 * 2, true, false);
     private static Image charactersTileset = new Image("/playersprites.png", 800*2, 600*2, true, false );
+    private static Image pokeTileset = new Image("/pokesprites.png", 496*2, 912*2, true, false );
     private static Map<String, Tile> tileMap = new HashMap<>();
     public static class Tile {
         public final int x, y, w, h;
         Tile(int i, int j) {
-            x = i * (TILE_WIDTH);
-            y = j * (TILE_WIDTH);
-            w = TILE_WIDTH;
-            h = TILE_WIDTH;
+            x = i * (DEFAULT_TILE_WIDTH);
+            y = j * (DEFAULT_TILE_WIDTH);
+            w = DEFAULT_TILE_WIDTH;
+            h = DEFAULT_TILE_WIDTH;
         }
     }
 
@@ -55,24 +56,27 @@ public class Tiles {
         tileMap.put("empty", new Tile(0, 0));
         tileMap.put("wall", new Tile(7, 0));
         tileMap.put("floor", new Tile(1, 0));
-        tileMap.put("player", new Tile(27, 0));
+        tileMap.put("player", new Tile(1, 0));
         tileMap.put("skeleton", new Tile(29, 6));
         tileMap.put("lootbox", new Tile(12, 31));
         tileMap.put("slowpoke", new Tile(28, 6));
         tileMap.put("charizard", new Tile(27, 8));
-        tileMap.put("bulbasaur", new Tile(24,8));
+        tileMap.put("bulbasaur", new Tile(20,8));
 
     }
 
 
     public static void drawTile(GraphicsContext context, Drawable d, int x, int y) {
         Tile tile = tileMap.get(d.getTileName());
-        if (!d.getTileName().equals("player")) {
-            context.drawImage(backgroundTileset, tile.x, tile.y, tile.w, tile.h,
-                    x * TILE_WIDTH, y * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
-        } else {
+        if (d.getTileName().equals("player")) {
             context.drawImage(charactersTileset, tile.x, tile.y, tile.w, tile.h,
-                    x * TILE_WIDTH, y * TILE_WIDTH, TILE_WIDTH, TILE_WIDTH);
+                    x * DEFAULT_TILE_WIDTH, y * DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH);
+        } else if (d.getTileName().matches("charizard|slowpoke|bulbasaur")){
+            context.drawImage(pokeTileset, tile.x, tile.y, tile.w, tile.h,
+                    x * DEFAULT_TILE_WIDTH, y * DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH);
+        } else {
+            context.drawImage(backgroundTileset, tile.x, tile.y, tile.w, tile.h,
+                    x * DEFAULT_TILE_WIDTH, y * DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH, DEFAULT_TILE_WIDTH);
         }
     }
 }
