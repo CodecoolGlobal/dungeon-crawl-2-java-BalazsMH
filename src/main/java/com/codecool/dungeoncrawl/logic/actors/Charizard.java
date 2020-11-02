edd62402eef3b967751134bafc408e463c14dca1
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-02 10:44:35 +0100

    Add subclass to Pokemon

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Charizard.java
@@ -0,0 +1,16 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Charizard extends Pokemon {
+
+    public Charizard(Cell cell, String name){
+        super(cell, name);
+    }
+    @Override
+    public void move(){}
+    @Override
+    public void fight(){}
+
+}
