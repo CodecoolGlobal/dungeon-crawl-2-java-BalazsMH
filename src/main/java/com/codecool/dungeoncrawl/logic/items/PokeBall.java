ca0929fe812ee2beb79338f3bf331b06b9df4291
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-03 16:03:56 +0100

    Change capture rate in PokeBall constructor

--- src/main/java/com/codecool/dungeoncrawl/logic/items/PokeBall.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/items/PokeBall.java
@@ -4,8 +4,8 @@ public class PokeBall {
     private final int catchRate;
 
     public PokeBall() {
-        this.catchRate = (int) Math.floor(Math.random()*10.00);
+        this.catchRate = (int) Math.floor(Math.random()*(8 - 2) + 2);
     }
 
-
+    public int getCatchRate() { return catchRate; }
 }
