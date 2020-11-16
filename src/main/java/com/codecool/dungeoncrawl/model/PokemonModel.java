29088a6642b8bc7d7e6bb94450e9bdcb5fed0426
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-16 14:41:48 +0100

    Add getters and setters to PokemonModel

--- src/main/java/com/codecool/dungeoncrawl/model/PokemonModel.java
+++ src/main/java/com/codecool/dungeoncrawl/model/PokemonModel.java
@@ -4,7 +4,6 @@ import com.codecool.dungeoncrawl.logic.actors.pokemon.Pokemon;
 
 
 public class PokemonModel extends BaseModel{
-    private boolean isActive = false;
     private int pokeHealth;
     private int pokeDamage;
     private String pokeName;
@@ -20,5 +19,53 @@ public class PokemonModel extends BaseModel{
         y = pokemon.getY();
         cellType = pokemon.getCell().getType().name();
         this.setId((int)(Math.random()*1000000));
+    }
+
+    public int getPokeHealth() {
+        return pokeHealth;
+    }
+
+    public void setPokeHealth(int pokeHealth) {
+        this.pokeHealth = pokeHealth;
+    }
+
+    public int getPokeDamage() {
+        return pokeDamage;
+    }
+
+    public void setPokeDamage(int pokeDamage) {
+        this.pokeDamage = pokeDamage;
+    }
+
+    public String getPokeName() {
+        return pokeName;
+    }
+
+    public void setPokeName(String pokeName) {
+        this.pokeName = pokeName;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public String getCellType() {
+        return cellType;
+    }
+
+    public void setCellType(String cellType) {
+        this.cellType = cellType;
     }
 }
