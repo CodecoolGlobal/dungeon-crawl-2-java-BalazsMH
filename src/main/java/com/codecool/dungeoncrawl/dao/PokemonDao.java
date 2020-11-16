41f3b18eb690baeeb234e1dd68e28b8838b7ab78
FruzsinaElekes <elekes.fr@gmail.com>
2020-11-16 18:19:17 +0100

    Add db details to connection, savePokemon() method

--- src/main/java/com/codecool/dungeoncrawl/dao/PokemonDao.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/PokemonDao.java
@@ -5,7 +5,7 @@ import com.codecool.dungeoncrawl.model.PokemonModel;
 import java.util.List;
 
 public interface PokemonDao {
-    void add(PokemonModel pokemon);
+    void add(PokemonModel pokemon, int playerId);
     void update(PokemonModel pokemon);
     PokemonModel get(int id);
     List<PokemonModel> getAll();
