14836521816973520cbb8877ad7c85b9a9c26433
BalazsMH <horvathb91@gmail.com>
2020-11-18 14:59:02 +0100

    Major refactor. Game working, with issues.

--- src/main/java/com/codecool/dungeoncrawl/Game.java
+++ src/main/java/com/codecool/dungeoncrawl/Game.java
@@ -1,13 +1,30 @@
 package com.codecool.dungeoncrawl;
 
+import com.codecool.dungeoncrawl.dao.Converter;
+import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.EndCondition;
 import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.items.Inventory;
+import com.codecool.dungeoncrawl.logic.items.Key;
 import com.codecool.dungeoncrawl.logic.map.GameMap;
 import com.codecool.dungeoncrawl.logic.map.MapChanger;
 import com.codecool.dungeoncrawl.logic.map.MapGenerator;
 import com.codecool.dungeoncrawl.logic.map.MapLoader;
+import com.codecool.dungeoncrawl.logic.ui.WindowElement;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.scene.Scene;
 import javafx.scene.canvas.GraphicsContext;
 
 import javafx.scene.canvas.*;
+import javafx.scene.control.Label;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
 import java.util.List;
 
 public class Game {
@@ -19,20 +36,195 @@ public class Game {
     private final MapChanger mapChanger;
     private final Canvas canvas;
     private final GraphicsContext context;
+    private Label nameLabel = new Label();
+    private Label inv = new Label();
+    private Label currentInfo = new Label();
+    private Label currentLevel = new Label();
+    private int activeMap = 1;
+    private StringBuilder text = new StringBuilder();
+    private Timeline enemyMove;
+
+
+
 
 
     public Game() {
         boolean m = MapGenerator.generateMap(1);
         this.map1 = MapLoader.loadMap(1);
         this.mapWallsLevel1 = map1.getWalls();
+        //TODO: figure out why it doesn't allow simply calling Mapgenerator with a void return value
+
+        boolean mapReady = MapGenerator.generateMap(2);
         this.map2 = MapLoader.loadMap(2);
         this.mapWallsLevel2 = map2.getWalls();
+
+        this.player = this.map1.getPlayer();
+
         this.mapChanger = new MapChanger(map1, map2);
         this.canvas = new Canvas(
                 map1.getWidth() * Tiles.DEFAULT_TILE_WIDTH,
                 map1.getHeight() * Tiles.DEFAULT_TILE_WIDTH);
         this.context = canvas.getGraphicsContext2D();
+        this.addEnemyMoveHandler();
+
+    }
+
+    public Scene showGameScene() {
+        GameMap map = this.activeMap == 1 ? this.map1 : this.map2;
+        WindowElement.setLabels(this.currentLevel, this.nameLabel, this.currentInfo, this.inv, map);
+
+
+        VBox rightPane = WindowElement.createRightPane(map.getPlayer().getInventory(), map, nameLabel, inv, currentInfo);
+        VBox levelBox = WindowElement.createLevelBox(currentLevel);
+        VBox bottom = WindowElement.createBottomBox();
+
+        this.nameLabel.setText(this.player.getUserName());
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(canvas);
+        borderPane.setBottom(bottom);
+        borderPane.setRight(rightPane);
+        borderPane.setTop(levelBox);
+
+        Scene scene = new Scene(borderPane);
+        scene.setOnKeyPressed(this::onKeyPressed); //TODO: Not good. Create new InputHandler class. this is a temporary solution
+        return scene;
     }
 
 
+
+    private void onKeyPressed(KeyEvent keyEvent) {
+        GameMap map = this.activeMap == 1 ? this.map1 : this.map2;
+        text.setLength(0);
+        Inventory inventory = map.getPlayer().getInventory();
+        Player player = map.getPlayer();
+        KeyCode keyPressed = keyEvent.getCode();
+        switch (keyPressed) {
+            case UP:
+                player.move(0, -1, "up");
+                break;
+            case DOWN:
+                player.move(0, 1, "down");
+                break;
+            case LEFT:
+                player.move(-1, 0, "left");
+                break;
+            case RIGHT:
+                player.move(1,0, "right");
+                break;
+            case R:
+                map.getRocketGrunt().releasePokemon(map);
+                break;
+            case T:
+                player.throwPokeBall(text, map.getPokemonInRange(currentInfo), map);
+                checkIfGameEnds(inventory);
+                break;
+            case E:
+                if (player.whatAmIStandingOn() instanceof Key){
+                    inventory.addKey(player.getCell());
+                    player.getCell().setItem(null);
+                } else {
+                    player.pickupItem(text);
+                }
+                break;
+            case O:
+                if (player.hasKey() && player.standingOnDoor()){
+                    player.openDoor();
+                    this.activeMap = this.activeMap == 1 ? 2 : 1;
+                    map = mapChanger.changeMap(map); //TODO: currently it does not place the player
+                }
+                break;
+            case A:
+                inventory.changeActivePokemon();
+                break;
+            case F:
+                player.fightPokemon(text, map.getPokemonInRange(currentInfo), map);
+                checkIfGameEnds(inventory);
+                break;
+            case H:
+                inventory.heal();
+                break;
+            case S:
+                // TODO should be Ctrl + S
+                Converter converter = new Converter(mapChanger.getActive(), mapChanger.getStored());
+                converter.run();
+        }
+        refresh(inventory);
+    }
+
+    private void refresh(Inventory inventory) {
+        GameMap map = this.activeMap == 1 ? this.map1 : this.map2;
+
+        context.setFill(Color.BLACK);
+        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+        WindowElement.refreshInfoWindow(text, currentInfo, map);
+        WindowElement.refreshLevelAndInventory(inventory, inv, currentLevel, map);
+        for (int x = 0; x < map.getWidth(); x++) {
+            for (int y = 0; y < map.getHeight(); y++) {
+                Cell cell = map.getCell(x, y);
+                Tiles.drawTile(context, cell, x, y);
+
+                if (cell.getItem() != null) {
+                    Tiles.drawTile(context, cell.getItem(), x, y);
+                }
+                if (cell.getDoor() != null) {
+                    Tiles.drawTile(context, cell.getDoor(), x, y);
+                }
+                if (cell.getActor() != null) {
+                    Tiles.drawTile(context, cell.getActor(), x, y);
+                }
+
+                if (cell.getPokemon() != null) {
+                    Tiles.drawTile(context, cell.getPokemon(), x, y);
+                }
+
+            }
+        }
+    }
+
+    public void checkIfGameEnds(Inventory inventory){
+        if (inventory.getActivePokemon() == null){
+            WindowElement.gameEndWindow(EndCondition.LOSE, Main.getpStage());
+        } else if (map2.getRocketGrunt().getRocketPokemonList().size() == 0 && map2.getRocketGrunt().getRocketPokemonOnBoard().size() == 0){
+            WindowElement.gameEndWindow(EndCondition.WIN, Main.getpStage());
+        }
+    }
+    private void addEnemyMoveHandler() {
+        GameMap map = this.activeMap == 1 ? this.map1 : this.map2;
+
+        enemyMove = new Timeline(
+                new KeyFrame(Duration.seconds(1), (event) -> {
+                    map.moveAllPokemon();
+                    refresh(map.getPlayer().getInventory()); }));
+        enemyMove.setCycleCount(Timeline.INDEFINITE);
+        enemyMove.play();
+    }
+
+
+
+
+
+    public int getActiveMap() {
+        return activeMap;
+    }
+
+    public void setActiveMap(int activeMap) {
+        this.activeMap = activeMap;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public GameMap getMap1() {
+        return map1;
+    }
+
+    public GameMap getMap2() {
+        return map2;
+    }
 }
--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -1,14 +1,7 @@
 package com.codecool.dungeoncrawl;
 
-import com.codecool.dungeoncrawl.dao.Converter;
-import com.codecool.dungeoncrawl.logic.*;
-import com.codecool.dungeoncrawl.logic.actors.Player;
-import com.codecool.dungeoncrawl.logic.items.Inventory;
-import com.codecool.dungeoncrawl.logic.items.Key;
 import com.codecool.dungeoncrawl.logic.map.MapGenerator;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import com.codecool.dungeoncrawl.logic.map.GameMap;
 import com.codecool.dungeoncrawl.logic.map.MapChanger;
 import com.codecool.dungeoncrawl.logic.map.MapLoader;
@@ -18,16 +11,10 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.util.Arrays;
 import java.util.List;
@@ -38,13 +25,7 @@ public class Main extends Application {
 
 
 
-    Timeline enemyMove;
 
-    Label nameLabel = new Label();
-    Label inv = new Label();
-    Label currentInfo = new Label();
-    Label currentLevel = new Label();
-    StringBuilder text = new StringBuilder();
     String[] developers = new String[]{"Fruzsi", "Dani", "Peti", "Balázs"};
 
     public static void main(String[] args) {
@@ -57,40 +38,20 @@ public class Main extends Application {
         primaryStage.setTitle("JavaMon");
         primaryStage.getIcons().add(new Image("file:logo.png"));
 
-        //TODO:game should only be created when the player selects new game.
-        //Scene game = gameScene();
         Scene mainMenu = mainMenu(primaryStage);
 
         primaryStage.setScene(mainMenu);
-        addEnemyMoveHandler();
         //refresh(map.getPlayer().getInventory()); why does it need to refresh right away?
         primaryStage.show();
     }
 
-    private void setupGame(){
-        boolean m = MapGenerator.generateMap(1);
-        GameMap map = MapLoader.loadMap(1);
-        List<List<Integer>> mapWallsLevel1 = MapLoader.getWalls();
-        boolean mapReady = MapGenerator.generateMap(2);
-        GameMap map2 = MapLoader.loadMap(2);
-        List<List<Integer>> mapWallsLevel2 = MapLoader.getWalls();
-        //TODO: figure out why it doesn't allow simply calling Mapgenerator with a void return value
-
-
-        MapChanger mapChanger = new MapChanger(map, map2);
-
-        Canvas canvas = new Canvas(
-                map.getWidth() * Tiles.DEFAULT_TILE_WIDTH,
-                map.getHeight() * Tiles.DEFAULT_TILE_WIDTH);
-        GraphicsContext context = canvas.getGraphicsContext2D();
-    }
 
     private Scene mainMenu(Stage primaryStage) {
         TextField nameInput = WindowElement.createNameInput();
         Button newGameButton = WindowElement.createNewGameButton();
         Button loadGameButton = WindowElement.createLoadGameButton();
         //TODO: implement functionality for load game button.
-        newGameButton.setOnMouseClicked((event)-> this.onSubmitPressed(primaryStage, game, nameInput));
+        newGameButton.setOnMouseClicked((event)-> this.onNewGamePressed(primaryStage, nameInput));
         loadGameButton.setOnMouseClicked((event)-> this.onLoadPressed(primaryStage));
         VBox mainPane = WindowElement.createMainPane(nameInput, newGameButton, loadGameButton);
         Scene mainMenu = new Scene(mainPane);
@@ -98,33 +59,17 @@ public class Main extends Application {
         return mainMenu;
     }
 
-    private Scene gameScene() {
-        WindowElement.setLabels(currentLevel, nameLabel, currentInfo, inv, map);
 
-        VBox rightPane = WindowElement.createRightPane(map.getPlayer().getInventory(), map, nameLabel, inv, currentInfo);
-        VBox levelBox = WindowElement.createLevelBox(currentLevel);
-        VBox bottom = WindowElement.createBottomBox();
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(canvas);
-        borderPane.setBottom(bottom);
-        borderPane.setRight(rightPane);
-        borderPane.setTop(levelBox);
-
-        Scene scene = new Scene(borderPane);
-        scene.setOnKeyPressed(this::onKeyPressed);
-        return scene;
-    }
-
-    private void onSubmitPressed(Stage primaryStage, Scene gameScene, TextField nameInput) {
-
+    private void onNewGamePressed(Stage primaryStage,TextField nameInput) {
+        Game game = new Game();
         String enteredName = nameInput.getText();
-        map.getPlayer().setUserName(enteredName);
+        game.getMap1().getPlayer().setUserName(enteredName);
+
         if (Arrays.asList(developers).contains(enteredName)) {
-            map.getPlayer().setSuperUser(true);
+            game.getMap1().getPlayer().setSuperUser(true);
         }
-        nameLabel.setText(map.getPlayer().getUserName());
-        primaryStage.setScene(gameScene);
+
+        primaryStage.setScene(game.showGameScene());
     }
 
     private void onLoadPressed(Stage primaryStage) {
@@ -132,104 +77,7 @@ public class Main extends Application {
         primaryStage.setScene(loadGameScene);
     }
 
-    private void onKeyPressed(KeyEvent keyEvent) {
-        text.setLength(0);
-        Inventory inventory = map.getPlayer().getInventory();
-        Player player = map.getPlayer();
-        KeyCode keyPressed = keyEvent.getCode();
-        switch (keyPressed) {
-            case UP:
-                player.move(0, -1, "up");
-                break;
-            case DOWN:
-                player.move(0, 1, "down");
-                break;
-            case LEFT:
-                player.move(-1, 0, "left");
-                break;
-            case RIGHT:
-                player.move(1,0, "right");
-                break;
-            case R:
-                map.getRocketGrunt().releasePokemon(map);
-                break;
-            case T:
-                player.throwPokeBall(text, map.getPokemonInRange(currentInfo), map);
-                checkIfGameEnds(inventory);
-                break;
-            case E:
-                if (player.whatAmIStandingOn() instanceof Key){
-                    inventory.addKey(player.getCell());
-                    player.getCell().setItem(null);
-                } else {
-                    player.pickupItem(text);
-                }
-                break;
-            case O:
-                if (player.hasKey() && player.standingOnDoor()){
-                    player.openDoor();
-                    map = mapChanger.changeMap(map);
-                }
-                break;
-            case A:
-                inventory.changeActivePokemon();
-                break;
-            case F:
-                player.fightPokemon(text, map.getPokemonInRange(currentInfo), map);
-                checkIfGameEnds(inventory);
-                break;
-            case H:
-                inventory.heal();
-                break;
-            case S:
-                // TODO should be Ctrl + S
-                Converter converter = new Converter(mapChanger.getActive(), mapChanger.getStored());
-                converter.run();
-        }
-        refresh(inventory);
-    }
-
-    private void refresh(Inventory inventory) {
-        context.setFill(Color.BLACK);
-        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-        WindowElement.refreshInfoWindow(text, currentInfo, map);
-        WindowElement.refreshLevelAndInventory(inventory, inv, currentLevel, map);
-        for (int x = 0; x < map.getWidth(); x++) {
-            for (int y = 0; y < map.getHeight(); y++) {
-                Cell cell = map.getCell(x, y);
-                Tiles.drawTile(context, cell, x, y);
-
-                if (cell.getItem() != null){
-                    Tiles.drawTile(context, cell.getItem(), x, y);
-                }
-                if(cell.getDoor() != null) {
-                    Tiles.drawTile(context, cell.getDoor(), x, y);
-                }
-                if (cell.getActor() != null) {
-                    Tiles.drawTile(context, cell.getActor(), x, y);
-                }
-
-                if(cell.getPokemon() != null){
-                    Tiles.drawTile(context, cell.getPokemon(), x, y);
-                }
-
-            }
-        }
-    }
-
-    public void checkIfGameEnds(Inventory inventory){
-        if (inventory.getActivePokemon() == null){
-            WindowElement.gameEndWindow(EndCondition.LOSE, pStage);
-        } else if (map2.getRocketGrunt().getRocketPokemonList().size() == 0 && map2.getRocketGrunt().getRocketPokemonOnBoard().size() == 0){
-            WindowElement.gameEndWindow(EndCondition.WIN, pStage);
-        }
-    }
-    private void addEnemyMoveHandler() {
-        enemyMove = new Timeline(
-                new KeyFrame(Duration.seconds(1), (event) -> {
-                    map.moveAllPokemon();
-                    refresh(map.getPlayer().getInventory()); }));
-        enemyMove.setCycleCount(Timeline.INDEFINITE);
-        enemyMove.play();
+    public static Stage getpStage() {
+        return pStage;
     }
 }
